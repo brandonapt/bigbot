c943a7160b3d5c2b6b98f6fa3f529cf68dc3d6e7
Brandon! <brandoge91@gmail.com>
2022-03-23 21:07:34 +0000

    fixed critial bug because im so stupid

--- utils/roblox/init.js
+++ utils/roblox/init.js
@@ -2,3 +2,9 @@ const roblox = require('noblox.js')
 require('dotenv').config()
 
 
+roblox.setCookie(process.env.COOKIE).then(async function() { //Use COOKIE from our .env file.
+    let person = await roblox.getCurrentUser()
+    console.log(`[NOBLOX] Logged in as ${person.UserName}!`)
+}).catch(function(err) {
+    console.log("[NOBLOX] Unable to log in!", err)
+})
